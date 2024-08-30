f1bea89aedecaf4ef40255724af46edc56dcdd08
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2024-08-30 15:42:19 +0100

    init

--- mod.ts
+++ mod.ts
@@ -1,3 +1,84 @@
-export function add(a: number, b: number): number {
-  return a + b;
+import type { PackageJson } from "type-fest";
+
+import process from "node:process";
+import { tmpdir } from "node:os";
+import { $ } from "@david/dax";
+import { download } from "nrd";
+import * as semver from "@std/semver";
+
+function getTagName(): string | undefined {
+  const githubRef = process.env.GITHUB_REF;
+  if (githubRef && githubRef.startsWith("refs/tags/")) {
+    return githubRef.replace("refs/tags/", "");
+  } else {
+    return undefined;
+  }
+}
+
+function getVersionStr(): string {
+  const tagName = getTagName();
+  if (tagName == null) {
+    throw new Error("Could not get tag name from GITHUB_REF");
+  }
+
+  const versionStr = tagName.replace(/^v/, "");
+  if (!semver.canParse(versionStr)) {
+    throw new Error(`Invalid version string: ${versionStr}`);
+  }
+  return versionStr;
+}
+
+/**
+ * ex. "@octocat/hello-world" -> "@jsr/octocat__hello-world"
+ */
+function convertToPackageName(name: string): string {
+  const [scope, pkg] = name.split("/");
+  if (!scope || !pkg) {
+    throw new Error(`Invalid package name: ${name}`);
+  }
+  return `@jsr/${scope}__${pkg}`;
+}
+
+const tmpDir = $.path(tmpdir());
+
+if (import.meta.main) {
+  $.cd(tmpDir);
+
+  const packageName = convertToPackageName(process.env.PACKAGE_NAME ?? "");
+  const packageVersion = process.env.PACKAGE_VERSION ?? getVersionStr();
+
+  download(packageName, {
+    registry: "https://npm.jsr.io",
+    version: packageVersion,
+  });
+
+  $.cd(tmpDir.join("./package"));
+
+  const pkgJson = await $`cat ./package.json`.json() as PackageJson;
+
+  const versionStr = process.env.PACKAGE_VERSION ?? getVersionStr();
+
+  if (pkgJson.version !== versionStr) {
+    throw new Error("Version mismatch");
+  }
+
+  pkgJson.name = process.env.PACKAGE_NAME;
+  pkgJson.description = process.env.PACKAGE_DESCRIPTION;
+  pkgJson.homepage = process.env.PACKAGE_HOMEPAGE ?? pkgJson.homepage;
+  pkgJson.repository = process.env.PACKAGE_REPOSITORY ??
+    process.env.GITHUB_REPOSITORY;
+  pkgJson.license = process.env.PACKAGE_LICENSE ?? "MIT";
+  pkgJson.author = process.env.PACKAGE_AUTHOR ??
+    process.env.GITHUB_REPOSITORY_OWNER;
+
+  /* remove undefined fields */
+  Object.keys(pkgJson).forEach((key) => {
+    if (pkgJson[key] == null) {
+      delete pkgJson[key];
+    }
+  });
+
+  await $`echo ${JSON.stringify(pkgJson, null, 2)} > ./package.json`;
+
+  await $`npm publish ${process.env.NPM_OPTIONS ?? "--access public"}`;
 }
--- mod_test.ts
+++
@@ -1,8 +0,0 @@
-import { test } from "@cross/test";
-import { assertEquals } from "@std/assert";
-
-import { add } from "./mod.ts";
-
-test("dummy", () => {
-  assertEquals(add(1, 2), 3);
-});
